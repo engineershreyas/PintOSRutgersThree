1a288d3f188793b04d8175fb0969b1541f075c79
chb50 <cedricblake48@gmail.com>
2016-04-26 14:00:14 -0400

    passes another test! 28 now fail

--- src/vm/spage.h
+++ src/vm/spage.h
@@ -8,7 +8,7 @@
 #include "vm/frame.h"
 
 struct spage_table {
-	uint32_t* pt_ptr //pointer to corresponding page on the page table
+	uint32_t* pt_ptr; //pointer to corresponding page on the page table
 
 	void* data_to_fetch; // if a page has a page fault due to unaccessed data, we use this to store data from the file system, swap slot, or all zero page
 	bool valid_access; //if valid_access = 0; then we terminate the process and free the resources of the page that pt_ptr points to (it tried to access an invalid address)
