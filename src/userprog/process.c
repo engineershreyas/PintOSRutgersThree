138c1932a127c9572456638e90be6478f136a227
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-05-02 17:14:55 -0400

    destroyed spage table

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -184,6 +184,8 @@ process_exit (void)
       free (file_d);
     }
 
+    spage_table_destroy(&cur->supp_page_table);
+
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
